47189fc9c609a5704ebbe52c6a47674480fd616a
Ben <iofelben+github@gmail.com>
2015-05-20 23:00:02 -0400

    threads

--- Makefile
+++ Makefile
@@ -1,3 +1,4 @@
 CXX = clang++
 CXXFLAGS = -std=c++11 -g -Wall
+LDFLAGS = -pthread
 all: main
--- main.cc
+++ main.cc
@@ -1,38 +1,50 @@
 #include "ttt3d.h"
 
 #include <cstdio>
-#include <thread>
 #include <cinttypes>
+#include <thread>
 
-namespace BP {
+namespace BenAndPrinceton {
 
 enum BoardItem {
     EMPTY,
-    P1, // me
-    P2
+    US, // me
+    THEM
 };
 
 using Board = BoardItem[4][4][4];
 
-struct AI : public TTT3D {
+class AI : public TTT3D {
+public:
     explicit AI(const duration<double> tta) : TTT3D(tta) {
+    }
+
+    ~AI() {
+        thread.join();
     }
 
     void next_move(int mv[3]) {
         if (mv[0] != -1)
-            game_board[mv[0]][mv[1]][mv[2]] = P2;
+            game_board[mv[0]][mv[1]][mv[2]] = THEM;
 
         // compute move
 
-        game_board[mv[0]][mv[1]][mv[2]] = P1;
+        game_board[mv[0]][mv[1]][mv[2]] = US;
     }
 
-    Board game_board = {{{EMPTY}}};
+private:
+    void compute_game_tree() {
+    }
+
+    std::thread thread = std::thread(&AI::compute_game_tree, this);
+
+    Board game_board;
 };
 
 }
 
 int main() {
     auto length = minutes(3);
-    auto ai = new BP::AI(length);
+    BenAndPrinceton::AI ai(length);
+    ai.sqzzl((int[]){0,0,0});
 }
