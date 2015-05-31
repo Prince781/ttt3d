87ebe3c08eab1262a0624eb62e901738d8d279a4
Prince781 <princetonf@optonline.net>
2015-05-31 17:28:48 -0400

    Wrote heuristic for get_weight. Not tested yet. See comments for details.

--- main.cc
+++ main.cc
@@ -10,90 +10,97 @@
 namespace BP {
 
 static const uint64_t wins[] = {
-               0xf,
-              0xf0,
-             0xf00,
-            0xf000,
-           0xf0000,
-          0xf00000,
-         0xf000000,
-        0xf0000000,
-       0xf00000000,
-      0xf000000000,
-     0xf0000000000,
-    0xf00000000000,
-   0xf000000000000,
-  0xf0000000000000,
- 0xf00000000000000,
-0xf000000000000000,
-   0x1000100010001,
-   0x2000200020002,
-   0x4000400040004,
-   0x8000800080008,
-  0x10001000100010,
-  0x20002000200020,
-  0x40004000400040,
-  0x80008000800080,
- 0x100010001000100,
- 0x200020002000200,
- 0x400040004000400,
- 0x800080008000800,
-0x1000100010001000,
-0x2000200020002000,
-0x4000400040004000,
-0x8000800080008000,
-            0x1111,
-            0x2222,
-            0x4444,
-            0x8888,
-        0x11110000,
-        0x22220000,
-        0x44440000,
-        0x88880000,
-    0x111100000000,
-    0x222200000000,
-    0x444400000000,
-    0x888800000000,
-0x1111000000000000,
-0x2222000000000000,
-0x4444000000000000,
-0x8888000000000000,
-0x1000010000100001,
-0x2000020000200002,
-0x4000040000400004,
-0x8000080000800008,
-   0x8000400020001,
-  0x80004000200010,
- 0x800040002000100,
-0x8000400020001000,
-            0x8421,
-        0x84210000,
-    0x842100000000,
-0x8421000000000000,
-   0x1001001001000,
-   0x2002002002000,
-   0x4004004004000,
-   0x8008008008000,
-   0x1000200040008,
-  0x10002000400080,
- 0x100020004000800,
-0x1000200040008000,
-            0x1248,
-        0x12480000,
-    0x124800000000,
-0x1248000000000000,
-0x8000040000200001,
-   0x1002004008000,
-   0x8004002001000,
-0x1000020000400008
+		       0xf,
+		      0xf0,
+		     0xf00,
+		    0xf000,
+		   0xf0000,
+		  0xf00000,
+		 0xf000000,
+		0xf0000000,
+	       0xf00000000,
+	      0xf000000000,
+	     0xf0000000000,
+	    0xf00000000000,
+	   0xf000000000000,
+	  0xf0000000000000,
+	 0xf00000000000000,
+	0xf000000000000000,
+	   0x1000100010001,
+	   0x2000200020002,
+	   0x4000400040004,
+	   0x8000800080008,
+	  0x10001000100010,
+	  0x20002000200020,
+	  0x40004000400040,
+	  0x80008000800080,
+	 0x100010001000100,
+	 0x200020002000200,
+	 0x400040004000400,
+	 0x800080008000800,
+	0x1000100010001000,
+	0x2000200020002000,
+	0x4000400040004000,
+	0x8000800080008000,
+		    0x1111,
+		    0x2222,
+		    0x4444,
+		    0x8888,
+		0x11110000,
+		0x22220000,
+		0x44440000,
+		0x88880000,
+	    0x111100000000,
+	    0x222200000000,
+	    0x444400000000,
+	    0x888800000000,
+	0x1111000000000000,
+	0x2222000000000000,
+	0x4444000000000000,
+	0x8888000000000000,
+	0x1000010000100001,
+	0x2000020000200002,
+	0x4000040000400004,
+	0x8000080000800008,
+	   0x8000400020001,
+	  0x80004000200010,
+	 0x800040002000100,
+	0x8000400020001000,
+		    0x8421,
+		0x84210000,
+	    0x842100000000,
+	0x8421000000000000,
+	   0x1001001001000,
+	   0x2002002002000,
+	   0x4004004004000,
+	   0x8008008008000,
+	   0x1000200040008,
+	  0x10002000400080,
+	 0x100020004000800,
+	0x1000200040008000,
+		    0x1248,
+		0x12480000,
+	    0x124800000000,
+	0x1248000000000000,
+	0x8000040000200001,
+	   0x1002004008000,
+	   0x8004002001000,
+	0x1000020000400008
 };
 
 enum Player { NONE, US, THEM };
 struct Board {
     uint64_t us = 0, them = 0;
 
-    static uint64_t mask(int x, int y, int z){
+    static uint64_t mask(int x, int y, int z) {
         return 1UL << (x*16 + y*4 + z);
+    }
+
+    static int numbits(uint64_t val) {
+	int i;
+	for (i=0; val; ++i)
+		val &= val - 1;
+	return i;
     }
 
     void set(Player p, int x, int y, int z) {
@@ -125,11 +132,15 @@ struct Board {
                 return THEM;
         return NONE;
     }
+
+    uint64_t getEmpty() {
+	return ~(us | them);
+    }
 };
 
 struct best {
     int x, y, z;
-    int score;
+    float score;
 };
 
 // TODO: const stuff
@@ -144,16 +155,60 @@ struct AI : public TTT3D {
 #endif
     }
 
-    int get_weight(Board b) {
-        // TODO
-        return 0;
+    float get_weight(Board b, Player p) {
+	assert(p == US || p == THEM);
+	const Player winner = b.win();
+	const uint64_t empty = b.getEmpty();
+	float w;
+
+	/*
+	 * weight = (# of n-steps to victory) / n
+	 * n is smallest number of steps to victory
+	 * rationale:
+	 * - more ways to win > less ways to win (because other player has to defend more)
+	 * - smaller number of steps (n) to win > larger number of steps to win
+	 */
+	if (winner == US)
+		w = INFINITY;	// infinite ways to win in zero steps
+	else if (winner == THEM)
+		w = -INFINITY;
+	else {
+		int us_min_n = 4;	// step length
+		int us_ways = 0;
+		int them_min_n = 4;
+		int them_ways = 0;
+		// get n for US
+		for (int i=0; i<76; ++i)
+			if (!(wins[i] & b.them)) { // wins[i] in empty or b.us
+				uint64_t unoccupied = wins[i] & empty;
+				int n = numbits(unoccupied);
+				if (n < us_min_n) {
+					us_min_n = n;
+					us_ways = 1;
+				} else if (n == us_min_n)
+					++us_ways;
+			} else if (!(wins[i] & b.us)) { // wins[i] in empty or b.them
+				uint64_t unoccupied = wins[i] & empty;
+				int n = numbits(unoccupied);
+				if (n < them_min_n) {
+					them_min_n = n;
+					them_ways = 1;
+				} else if (n == them_min_n)
+					++them_ways;
+			}
+		float w_us = (float) us_ways / us_min_n;
+		float w_them = (float) them_ways / them_min_n;
+		w = w_us - w_them;
+	}
+	return (p == US ? w : -w);
     }
 
     const int MAX_DEPTH = 10;
 
-    best get_best_move(Board b, Player t, int depth = 0) {
+    best get_best_move(Board b, Player t, 
+		       int moveX = -1, int moveY = -1, int moveZ = -1, int depth = 0) {
         if (depth == MAX_DEPTH)
-            return (best){-1,-1,-1, get_weight(b)};
+            return (best){moveX, moveY, moveZ, get_weight(b, t)};
 
         /*
          * terminal node
@@ -168,20 +223,22 @@ struct AI : public TTT3D {
                     if (b.get(x,y,z) == NONE) {
                         if (t == US) {
                             b.set(US,x,y,z);
-                            children.push_back(get_best_move(b, THEM, depth+1));
+                            children.push_back(get_best_move(b, THEM, x, y, z, depth+1));
                             b.set(NONE,x,y,z);
                         } else {
                             b.set(THEM,x,y,z);
-                            children.push_back(get_best_move(b, US, depth+1));
+                            children.push_back(get_best_move(b, US, x, y, z, depth+1));
                             b.set(NONE,x,y,z);
                         }
                     }
         }
         
         if (t == US)
-            return *std::max_element(children.begin(), children.end(), [](best a, best b) { return a.score < b.score; });
+            return *std::max_element(children.begin(), children.end(), 
+		[](best a, best b) { return a.score < b.score; });
         else
-            return *std::min_element(children.begin(), children.end(), [](best a, best b) { return a.score < b.score; });
+            return *std::min_element(children.begin(), children.end(), 
+		[](best a, best b) { return a.score < b.score; });
     }
 
     void next_move(int mv[3]) {
